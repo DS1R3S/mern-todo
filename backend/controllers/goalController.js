7c9f8eb575e75098a12fd551129f38613c527591
Daniel Sires <danielsires@Daniels-MacBook-Pro.local>
2022-02-18 15:09:22 -0500

    react components

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const { update } = require("../models/goalModel");
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 // get goals GET
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -15,6 +16,7 @@ const createGoal = asyncHandler(async (req, res) => {
   }
   const newGoal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
   res.status(200).json(newGoal);
 });
@@ -22,9 +24,21 @@ const createGoal = asyncHandler(async (req, res) => {
 // update goal PUT
 const updateGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
+
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+  const user = await User.findById(req.user.id);
+  // check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  // Make sure the logged in user matches the user goal
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -38,6 +52,17 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+  const user = await User.findById(req.user.id);
+  // check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  // Make sure the logged in user matches the user goal
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
   await goal.remove();
   res.status(200).json({ id: req.params.id });
