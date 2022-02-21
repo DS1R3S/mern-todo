fd8b4c04e5e3fc4e2b20cd7f834fd91fa3abc48c
Daniel Sires <danielsires@Daniels-MacBook-Pro.local>
2022-02-20 21:57:10 -0500

    fixed deploy

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-if (process.env.NODE.ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(
